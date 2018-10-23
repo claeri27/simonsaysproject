415c640a5b20290f688a97164906b8bbb08401da
Christopher Laeri <chris@ool-ad020560.dyn.optonline.net>
2018-10-23 11:21:52 -0400

    css+html looking good, js next

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+const startButton = document.querySelector('.start-button');
+const hardButton = document.querySelector('.hard-button');
+const turnText = document.querySelector('.turn-count');
+const titleText = document.querySelector('h1');
+const levelCount = document.querySelector('.level-count');
+const redButton = document.querySelector('#redButton');
+const blueButton = document.querySelector('#blueButton');
+const greenButton = document.querySelector('#greenButton');
+const purpleButton = document.querySelector('#purpleButton');
+const orangeButton = document.querySelector('#orangeButton');
+const yellowButton = document.querySelector('#yellowButton');
+
 let compPattern = [];
 let newCompMove;
 let playerInput;
+
+function startGame() {
+  
+}
